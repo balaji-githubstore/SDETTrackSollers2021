342882c7dd843c40e6701e88a745feaa8f18becb
balaji-githubstore <balaji0017@gmail.com>
2021-07-16 22:18:04 +0530

    Add project files.

---
+++ SeleniumWebDriverConcept/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace SeleniumWebDriverConcept123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();  //runtime polymorphism - method to be called is resolved during run time based on the object created.
+            driver.Url = "https://magento.com/";
+
+            IWebElement signInElement= driver.FindElement(By.Id("gnav_557"));
+            signInElement.Click();
+
+            IWebElement emailElement= driver.FindElement(By.Id("email"));
+            emailElement.SendKeys("balaji0017@gmail.com");
+
+            driver.FindElement(By.Id("pass")).SendKeys("balaji@12345");
+
+            //click on continue
+
+            
+
+           //driver.Quit();
+
+        }
+    }
+}
