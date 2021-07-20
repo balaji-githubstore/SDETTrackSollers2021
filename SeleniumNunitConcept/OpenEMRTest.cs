44caaa4b306363f1d22a0d07ab33b59a533a71ec
balaji-githubstore <balaji0017@gmail.com>
2021-07-20 23:34:23 +0530

    explicit / fluent wait / assert true,equal, add patient

--- SeleniumNunitConcept/OpenEMRTest.cs
+++ SeleniumNunitConcept/OpenEMRTest.cs
@@ -104,7 +104,7 @@ namespace SeleniumNunitConcept.OpenEmr
             driver.FindElement(By.XPath("//input[@value='Confirm Create New Patient']")).Click();
             driver.SwitchTo().DefaultContent();
 
-            //explicit wait
+            //fluent wait
             DefaultWait<IWebDriver> wait = new DefaultWait<IWebDriver>(driver);
             wait.Timeout = TimeSpan.FromSeconds(50);
             wait.PollingInterval = TimeSpan.FromSeconds(5);
@@ -118,5 +118,90 @@ namespace SeleniumNunitConcept.OpenEmr
 
 
         }
+        [Test]
+        public void AddPatient2Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "http://demo.openemr.io/b/openemr/interface/login/login.php?site=default";
+
+            driver.FindElement(By.CssSelector("#authUser")).SendKeys("admin");
+            driver.FindElement(By.Id("clearPass")).SendKeys("pass");
+
+            SelectElement select = new SelectElement(driver.FindElement(By.Name("languageChoice")));
+            select.SelectByText("English (Indian)");
+            //click login
+
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+            driver.FindElement(By.XPath("//div[text()='Patient/Client']")).Click();
+            driver.FindElement(By.XPath("//div[text()='Patients']")).Click();
+
+            driver.SwitchTo().Frame("fin");
+            driver.FindElement(By.Id("create_patient_btn1")).Click();
+            driver.SwitchTo().DefaultContent();
+
+            driver.SwitchTo().Frame("pat");
+
+            driver.FindElement(By.Id("form_fname")).SendKeys("bala");
+            driver.FindElement(By.Id("form_lname")).SendKeys("dina");
+            driver.FindElement(By.Id("form_DOB")).SendKeys("2021-07-19");
+            SelectElement selectGender = new SelectElement(driver.FindElement(By.Id("form_sex")));
+            selectGender.SelectByText("Female");
+            driver.FindElement(By.Id("create")).Click();
+
+            driver.SwitchTo().DefaultContent();
+
+            driver.SwitchTo().Frame("modalframe");
+            driver.FindElement(By.XPath("//input[@value='Confirm Create New Patient']")).Click();
+            driver.SwitchTo().DefaultContent();
+
+            //fluent wait
+            DefaultWait<IWebDriver> wait = new DefaultWait<IWebDriver>(driver);
+            wait.Timeout = TimeSpan.FromSeconds(50);
+            wait.PollingInterval = TimeSpan.FromSeconds(5);
+            wait.IgnoreExceptionTypes(typeof(NoAlertPresentException));//ignore no alert exception
+
+            string actualValueOfAlert = wait.Until(x => x.SwitchTo().Alert()).Text;
+            Console.WriteLine(actualValueOfAlert);
+
+            wait.Until(x => x.SwitchTo().Alert()).Accept();
+
+            if(driver.FindElements(By.XPath("//div[@class='closeDlgIframe']")).Count>0) // check element present or not
+            {
+                driver.FindElement(By.XPath("//div[@class='closeDlgIframe']")).Click();
+            }
+            //driver.SwitchTo().Frame("Pat");
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[contains(@src,'new.php')]")));
+
+           String actualValueOfAddedPatient= driver.FindElement(By.XPath("//h2[contains(text(),'Medical')]")).Text;
+
+
+            Assert.IsTrue(actualValueOfAlert.Contains("Assessment: Tobacco"));//given condition must be true otherwise method is failure
+
+            Assert.AreEqual("Medical Record Dashboard - Bala Dina", actualValueOfAddedPatient);
+
+        }
+
+
+        //if(driver.FindElement(By.XPath("//div[@class='closeDlgIframe']")).Displayed)
+        //{
+        //    //
+        //}
+
+
+        //public bool IsElementPresent()
+        //{
+        //    try
+        //    {
+        //        driver.FindElement(By.XPath("//div[@class='closeDlgIframe']"));
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false
+        //    }
+        //}
     }
 }
