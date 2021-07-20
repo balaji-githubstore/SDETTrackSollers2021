da0ef8026c084465521e4ab8161e7b9ea7f3fd93
balaji-githubstore <balaji0017@gmail.com>
2021-07-21 00:03:06 +0530

    web table

--- SeleniumNunitConcept/WebTableTest.cs
+++ SeleniumNunitConcept/WebTableTest.cs
@@ -28,21 +28,45 @@ namespace SeleniumNunitConcept
         [Test]
         public void PrintAllNameTest()
         {
-            //IWebDriver driver = new ChromeDriver();
-            //driver.Manage().Window.Maximize();
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-            //driver.Url = "https://datatables.net/extensions/select/examples/initialisation/checkbox.html";
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://datatables.net/extensions/select/examples/initialisation/checkbox.html";
 
-            //string name = driver.FindElement(By.XPath("//table[@id='example']/tbody/tr[i]/td[2]")).Text;
-            //Console.WriteLine(name);
-
-            for(int i=1;i<=10;i++)
+            int rowCount = driver.FindElements(By.XPath("//table[@id='example']/tbody/tr")).Count;
+            Console.WriteLine(rowCount);
+            for (int i=1;i<= rowCount; i++)
             {
-                Console.WriteLine("i value = "+i+" kkkk" );
-
-                Console.WriteLine("//table[@id='example']/tbody/tr["+i+"]/td[2]");
+                string name = driver.FindElement(By.XPath("//table[@id='example']/tbody/tr["+i+"]/td[2]")).Text;
+                string sal = driver.FindElement(By.XPath("//table[@id='example']/tbody/tr[" + i + "]/td[6]")).Text;
+                Console.WriteLine(name+" & "+sal);
             }
            
+        }
+
+        [Test]
+        public void WorkingOnTableTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://datatables.net/extensions/select/examples/initialisation/checkbox.html";
+
+            int rowCount = driver.FindElements(By.XPath("//table[@id='example']/tbody/tr")).Count;
+
+            Console.WriteLine(rowCount);
+            for (int i = 1; i <= rowCount; i++)
+            {
+                string name = driver.FindElement(By.XPath("//table[@id='example']/tbody/tr[" + i + "]/td[2]")).Text;
+                
+                if(name.Equals("Brenden Wagner"))
+                {
+                    Console.WriteLine(driver.FindElement(By.XPath("//table[@id='example']/tbody/tr[" + i + "]/td[6]")).Text);
+                    driver.FindElement(By.XPath("//table[@id='example']/tbody/tr[" + i + "]/td[1]")).Click();
+                    break;
+                }
+            }
+
         }
     }
 }
